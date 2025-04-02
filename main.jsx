c70f93c90d5da2913309d9269cf9dfa991190c90
Swetha9126 <swethamurugesan062@gmail.com>
2025-04-02 10:02:45 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import Web from './Web.jsx'
 import Party from './party'
-//import Emoji from './emoji'
-//import Calculator from './Calculator/Calculator'
 import JsonExample from './jsonexample'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
